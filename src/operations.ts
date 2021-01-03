85ecf4d83a17da3bf8ace14eb222cf7094f3b5d6
Jack McCown <jackmac79@gmail.com>
2021-01-03 01:45:41 -0500

    deprecate op in favor of surfing keys
    their's is better

--- src/operations.ts
+++ src/operations.ts
@@ -8,36 +8,6 @@ import { extractTableAsJson } from "./helpers/extractTableInfo";
 
 const OPERATIONS: ReadonlyArray<Operation<any>> = [
   operation({
-    description: "Insert copy tooltip on codeblocks",
-    condition: ALWAYS,
-    // dependencies: { heading: SITE.SELECTOR_HEADING },
-    // action: e => {
-    //     e.heading.textContent = "Working userscript!"
-    // },
-    action: () => {
-      const preventDuplicateCopyButtons = (
-        elements: HTMLElement[]
-      ): HTMLElement[] => {
-        const siteHost = window.location.hostname;
-        if (siteHost.endsWith("gitlab.com")) {
-          return [];
-        }
-        // COULDDO filter elements based on siblings with copy in a className
-      };
-      const eligibleCodeElements = preventDuplicateCopyButtons(
-        document.querySelectorAll("code")
-      );
-
-      appendBtnToElements(
-        eligibleCodeElements,
-        (codeBlock) => addToClipboard(codeBlock.innerText),
-        (spanEl) => {
-          spanEl.innerText = "Copy";
-        }
-      );
-    },
-  }),
-  operation({
     description: "Insert a copy tooltip on tables",
     condition: ALWAYS,
     action: () => {
