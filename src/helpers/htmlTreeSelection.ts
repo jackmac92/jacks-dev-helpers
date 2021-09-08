bc7e97271120dd0673a3a6d45a7f6f483fe50b2b
Jack McCown <jackmac79@gmail.com>
2021-09-07 21:03:37 -0400

    tree select returns a promise resolving html node

--- src/helpers/htmlTreeSelection.ts
+++ src/helpers/htmlTreeSelection.ts
@@ -1,77 +1,129 @@
-export function htmlTreeSelection() {
-  // TODO select via keyboard?
-  // No need to wait for a 'DOMContentLoaded' event since the manifest
-  // specifies:
-  //
-  //   "run_at": "document_end"
-  //
+export function htmlTreeSelection(): Promise<HTMLElement> {
+  return new Promise((resolve) => {
+    // TODO select via keyboard?
+    // No need to wait for a 'DOMContentLoaded' event since the manifest
+    // specifies:
+    //
+    //   "run_at": "document_end"
+    //
 
-  let clickable = [
-    document.getElementsByTagName("a"),
-    document.getElementsByTagName("button"),
-    document.querySelectorAll("[role=button]"),
-  ];
-  const iframeOverlays: {
-    iframe: HTMLIFrameElement;
-    overlay: HTMLDivElement;
-  }[] = [];
+    let clickable = [
+      document.getElementsByTagName("a"),
+      document.getElementsByTagName("button"),
+      document.querySelectorAll("[role=button]"),
+    ];
+    const iframeOverlays: {
+      iframe: HTMLIFrameElement;
+      overlay: HTMLDivElement;
+    }[] = [];
 
-  let overHandler = (e: Event) => {
-    let target = e.target as HTMLElement;
+    let overHandler = (e: Event) => {
+      let target = e.target as HTMLElement;
 
-    if (target === null) {
-      return;
-    }
-
-    target.classList.add("html-tree-selection");
-
-    if (target.classList.contains("html-tree-selection-overlay")) {
-      if (target.dataset.index) {
-        let iframe = iframeOverlays[parseInt(target.dataset.index, 10)].iframe;
-        iframe.classList.add("html-tree-selection");
-      } else {
-        console.warn("failed to find iframe");
+      if (target === null) {
+        return;
       }
-    }
 
-    e.stopPropagation();
-  };
-  let outHandler = (e: Event) => {
-    let target = e.target as HTMLElement;
+      target.classList.add("html-tree-selection");
 
-    if (target === null) {
-      return;
-    }
-
-    target.classList.remove("html-tree-selection");
-    e.stopPropagation();
-  };
-
-  let clickHandler = (e: Event) => {
-    disable();
-
-    let target = e.target as HTMLElement;
-
-    if (target === null) {
-      return;
-    }
-
-    if (target.classList.contains("html-tree-selection-overlay")) {
-      if (target.dataset.index) {
-        target = iframeOverlays[parseInt(target.dataset.index, 10)].iframe;
-      } else {
-        console.warn("failed to find iframe");
+      if (target.classList.contains("html-tree-selection-overlay")) {
+        if (target.dataset.index) {
+          let iframe =
+            iframeOverlays[parseInt(target.dataset.index, 10)].iframe;
+          iframe.classList.add("html-tree-selection");
+        } else {
+          console.warn("failed to find iframe");
+        }
       }
-    }
 
-    // TODO instead of removing target, return it (or possibly many)
-    target.remove();
+      e.stopPropagation();
+    };
+    let outHandler = (e: Event) => {
+      let target = e.target as HTMLElement;
 
-    e.preventDefault();
-    e.stopPropagation();
-  };
+      if (target === null) {
+        return;
+      }
 
-  let enable = () => {
+      target.classList.remove("html-tree-selection");
+      e.stopPropagation();
+    };
+
+    let clickHandler = (e: Event) => {
+      disable();
+
+      let target = e.target as HTMLElement;
+
+      if (target === null) {
+        return;
+      }
+      e.preventDefault();
+      e.stopPropagation();
+
+      if (target.classList.contains("html-tree-selection-overlay")) {
+        if (target.dataset.index) {
+          target = iframeOverlays[parseInt(target.dataset.index, 10)].iframe;
+        } else {
+          console.warn("maybe failed to find iframe?", target, iframeOverlays);
+        }
+      }
+
+      resolve(target);
+    };
+
+    let disable = () => {
+      clickable.forEach((c) => {
+        for (var i = 0; i < c.length; i++) {
+          c[i].removeEventListener("click", clickHandler);
+          // @ts-expect-error
+          c[i].addEventListener("click", c[i].onclickBackup);
+          // @ts-expect-error
+          delete c[i].onclickBackup;
+        }
+      });
+
+      iframeOverlays.forEach((o) => o.overlay.remove());
+
+      document.documentElement.classList.remove("html-tree-selection-cursor");
+
+      // clean any orphaned hover applied class
+      let orphan = document.querySelector(".html-tree-selection");
+      if (orphan !== null) {
+        orphan.classList.remove("html-tree-selection");
+      }
+
+      document.removeEventListener("mouseover", overHandler);
+      document.removeEventListener("mouseout", outHandler);
+      document.removeEventListener("click", clickHandler);
+      document.removeEventListener("keydown", keyHandler, true);
+
+      window.removeEventListener("scroll", updateOverlayPositions);
+      window.removeEventListener("resize", updateOverlayPositions);
+    };
+
+    let keyHandler = (e: KeyboardEvent) => {
+      if (e.key === "Escape") {
+        disable();
+      }
+    };
+
+    let updateOverlayPositions = (_e: Event) => {
+      iframeOverlays.forEach((o) => {
+        let iframe = o.iframe;
+        let iframeClientRect = iframe.getBoundingClientRect();
+        let overlay = o.overlay;
+        let offsetX = iframeClientRect.left + window.scrollX;
+        let offsetY = iframeClientRect.top + window.scrollY;
+
+        overlay.style.top = `${offsetY}px`;
+        overlay.style.left = `${offsetX}px`;
+      });
+    };
+
+    // enable
+    window.addEventListener("scroll", updateOverlayPositions);
+    window.addEventListener("resize", updateOverlayPositions);
+
     // override click handlers on any clickable element
     clickable.forEach((c) => {
       for (var i = 0; i < c.length; i++) {
@@ -110,63 +162,5 @@ export function htmlTreeSelection() {
     document.addEventListener("mouseout", outHandler);
     document.addEventListener("click", clickHandler);
     document.addEventListener("keydown", keyHandler, true);
-  };
-
-  let disable = () => {
-    clickable.forEach((c) => {
-      for (var i = 0; i < c.length; i++) {
-        c[i].removeEventListener("click", clickHandler);
-        // @ts-expect-error
-        c[i].addEventListener("click", c[i].onclickBackup);
-        // @ts-expect-error
-        delete c[i].onclickBackup;
-      }
-    });
-
-    iframeOverlays.forEach((o) => o.overlay.remove());
-
-    document.documentElement.classList.remove("html-tree-selection-cursor");
-
-    // clean any orphaned hover applied class
-    let orphan = document.querySelector(".html-tree-selection");
-    if (orphan !== null) {
-      orphan.classList.remove("html-tree-selection");
-    }
-
-    document.removeEventListener("mouseover", overHandler);
-    document.removeEventListener("mouseout", outHandler);
-    document.removeEventListener("click", clickHandler);
-    document.removeEventListener("keydown", keyHandler, true);
-  };
-
-  let keyHandler = (e: KeyboardEvent) => {
-    if (e.key === "Escape") {
-      disable();
-    }
-  };
-
-  let updateOverlayPositions = (_e: Event) => {
-    iframeOverlays.forEach((o) => {
-      let iframe = o.iframe;
-      let iframeClientRect = iframe.getBoundingClientRect();
-      let overlay = o.overlay;
-      let offsetX = iframeClientRect.left + window.scrollX;
-      let offsetY = iframeClientRect.top + window.scrollY;
-
-      overlay.style.top = `${offsetY}px`;
-      overlay.style.left = `${offsetX}px`;
-    });
-  };
-
-  window.addEventListener("scroll", updateOverlayPositions);
-  window.addEventListener("resize", updateOverlayPositions);
-
-  return {
-    enable,
-    disable,
-    cleanup() {
-      window.removeEventListener("scroll", updateOverlayPositions);
-      window.removeEventListener("resize", updateOverlayPositions);
-    },
-  };
+  });
 }
